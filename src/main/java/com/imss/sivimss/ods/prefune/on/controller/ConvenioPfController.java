0b4425b865db9ae6676000fa537451087c66478c
Pablo Nolasco Castro <pablo.nolasco@people-media.com.mx>
2023-11-24 18:07:20 -0600

    Feat: add convenio

--- src/main/java/com/imss/sivimss/ods/prefune/on/controller/ConvenioPfController.java
+++ src/main/java/com/imss/sivimss/ods/prefune/on/controller/ConvenioPfController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,7 +54,7 @@ public class ConvenioPfController {
 
 	}
 	
-	@GetMapping("/detalle-convenio/{idConvenio}")
+	@PostMapping("/detalle-convenio/{idConvenio}")
 	@CircuitBreaker(name = "msflujo", fallbackMethod = "fallbackConsulta")
 	@Retry(name = "msflujo", fallbackMethod = "fallbackConsulta")
 	@TimeLimiter(name = "msflujo")
