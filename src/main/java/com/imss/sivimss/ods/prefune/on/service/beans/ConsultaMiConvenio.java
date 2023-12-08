5a1b6aeb53985fa890281d4dfd1cdde27d0abe20
LeandroPeopleMedia <107154425+LeandroPeopleMedia@users.noreply.github.com>
2023-12-08 11:44:07 -0600

    se modifica consulta

--- src/main/java/com/imss/sivimss/ods/prefune/on/service/beans/ConsultaMiConvenio.java
+++ src/main/java/com/imss/sivimss/ods/prefune/on/service/beans/ConsultaMiConvenio.java
@@ -56,7 +56,9 @@ public class ConsultaMiConvenio {
 						"SCPAC.ID_CONTRA_PAQ_CONVENIO_PF = SCBE.ID_CONTRA_PAQ_CONVENIO_PF ")
 				.where("SCPAC.ID_CONVENIO_PF = ".concat(idConvenio.toString()).concat(" AND SCBE.IND_ACTIVO =1 "));
 
-		selectQueryUtil.select("SCP.ID_CONVENIO_PF AS idConvenio", "SP.CVE_CURP AS curp",
+		selectQueryUtil.select("SCP.ID_CONVENIO_PF AS idConvenio",
+				"SC.ID_CONTRATANTE AS idContratante",
+				"SP.CVE_CURP AS curp",
 				"SCP.ID_VELATORIO AS idVelatorio",
 				"V.DES_VELATORIO AS velatorio",
 				"SECP.DES_ESTATUS AS estatus",
@@ -136,13 +138,14 @@ public class ConsultaMiConvenio {
 				.where("SF.ID_TIPO_ORDEN = 2").and("PF.ID_CONVENIO_PF = SCP.ID_CONVENIO_PF");
 		String subQuery = subQueryUtil.build();
 		queryUtil.select("SCP.ID_ESTATUS_CONVENIO AS idEstatusConvenio",
-				//"RPF.ID_ESTATUS AS estatusRenovacion",
+				// "RPF.ID_ESTATUS AS estatusRenovacion",
 				"IF(SCP.ID_TIPO_PREVISION=1, 'Plan Nuevo', 'Plan Anterior') AS previsionFuneraria",
 				"DATE_FORMAT(SCP.FEC_ALTA , '%d-%m-%Y') AS fecContratacion",
 				"IF(SCP.IND_RENOVACION=false, (DATE_FORMAT(SCP.FEC_VIGENCIA, '%d-%m-%Y')), DATE_FORMAT(RPF.FEC_VIGENCIA, '%d-%m-%Y')) AS fecVigencia",
 				"PAQ.MON_PRECIO AS cuotaRecuperacion",
 				"PAQ.REF_PAQUETE_NOMBRE AS tipoPaquete",
-				//"IF(SCP.IND_RENOVACION=false, ' ', DATE_FORMAT(RPF.FEC_ALTA, '%d-%m-%Y')) AS fecRenovacion",
+				// "IF(SCP.IND_RENOVACION=false, ' ', DATE_FORMAT(RPF.FEC_ALTA, '%d-%m-%Y')) AS
+				// fecRenovacion",
 				"IFNULL((".concat(subQuery) + "), FALSE) AS titularFallecido",
 				"DATE_FORMAT(CURDATE(), '%d-%m-%Y') AS fecActual",
 				"TIMESTAMPDIFF(DAY,IF(SCP.IND_RENOVACION=false, DATE_FORMAT(SCP.FEC_VIGENCIA, '%Y-%m-%01'), DATE_FORMAT(RPF.FEC_VIGENCIA, '%Y-%m-%01')), CURDATE()) AS diferenciaDias")
@@ -203,77 +206,73 @@ public class ConsultaMiConvenio {
 		log.info("renovacion: {}", query);
 		return query;
 	}
-	
-	public String consultarCurpRfc(String curp, String rfc) {
-        if (rfc.isEmpty() && rfc.equalsIgnoreCase("")) {
-                log.info("rfc vacio");
-                SelectQueryUtil selectQueryUtilCurp = new SelectQueryUtil();
-                selectQueryUtilCurp.select("SP.ID_PERSONA as idPersona", "SP.CVE_RFC AS rfc", "SP.CVE_CURP AS curp",
-                                "SP.CVE_NSS AS nss",
-                                "SP.NOM_PERSONA AS nomPersona",
-                                "SC.ID_CONTRATANTE as idDelContratante",
-                                "SP.NOM_PRIMER_APELLIDO AS primerApellido",
-                                "SP.NOM_SEGUNDO_APELLIDO AS segundoApellido",
-                                "SP.NUM_SEXO AS sexo", "IFNULL(SP.REF_OTRO_SEXO,'') AS otroSexo",
-                                "SP.FEC_NAC AS fechaNacimiento",
-                                "SP.ID_PAIS AS idPais", "SP.ID_ESTADO AS idEstado",
-                                "SP.REF_TELEFONO AS telefono", "SP.REF_CORREO AS correo",
-                                "SP.TIP_PERSONA AS tipoPersona",
-                                "SD.REF_CALLE AS calle", "SD.NUM_EXTERIOR AS numExterior",
-                                "SD.NUM_INTERIOR AS numInterior",
-                                "SD.REF_CP AS cp", "SD.REF_COLONIA AS colonia", "SD.REF_MUNICIPIO AS municipio",
-                                "SD.REF_ESTADO AS estado", "PA.DES_PAIS AS pais",
-                                "(SELECT COUNT(CPF.ID_CONTRA_PAQ_CONVENIO_PF) FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF WHERE CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE  LIMIT 1) AS tieneConvenio",
-                                "(SELECT C.DES_FOLIO  FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF LEFT JOIN SVT_CONVENIO_PF C ON CPF.ID_CONVENIO_PF = C.ID_CONVENIO_PF WHERE CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE  LIMIT 1) AS folioConvenio",
-                                "DATE_FORMAT((SELECT C.FEC_ALTA  FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF LEFT JOIN SVT_CONVENIO_PF C ON CPF.ID_CONVENIO_PF = C.ID_CONVENIO_PF where CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE  LIMIT 1 ),'%d/%m/%Y') AS fecha")
-                                .from("SVC_CONTRATANTE SC")
-                                .leftJoin("SVC_PERSONA SP", "SC.ID_PERSONA = SP.ID_PERSONA")
-                                .leftJoin("SVT_DOMICILIO SD", "SC.ID_DOMICILIO = SD.ID_DOMICILIO")
-                                .leftJoin("SVC_PAIS PA", "SP.ID_PAIS = PA.ID_PAIS")
-                                .where("SP.CVE_CURP = '" + curp+"'")
-                                .orderBy("SC.ID_CONTRATANTE DESC LIMIT 1");
-                query = selectQueryUtilCurp.build();
-                log.info(query);
-              
-                return query;
-        }
-        query  = busquedaRfcCurp(rfc);
-        return query;
-	}
-	
-	public String busquedaRfcCurp(String rfc) {
-        SelectQueryUtil selectQueryUtilRfc = new SelectQueryUtil();
-        selectQueryUtilRfc.select("SP.ID_PERSONA as idPersona", "SP.CVE_RFC AS rfc", "SP.CVE_CURP AS curp",
-                        "SP.CVE_NSS AS nss",
-                        "SP.NOM_PERSONA AS nomPersona",
-                        "SC.ID_CONTRATANTE as idDelContratante",
-                        "SP.NOM_PRIMER_APELLIDO AS primerApellido",
-                        "SP.NOM_SEGUNDO_APELLIDO AS segundoApellido",
-                        "SP.NUM_SEXO AS sexo", "IFNULL(SP.REF_OTRO_SEXO,'') AS otroSexo",
-                        "SP.FEC_NAC AS fechaNacimiento",
-                        "SP.ID_PAIS AS idPais", "SP.ID_ESTADO AS idEstado",
-                        "SP.REF_TELEFONO AS telefono", "SP.REF_CORREO AS correo",
-                        "SP.TIP_PERSONA AS tipoPersona",
-                        "SD.REF_CALLE AS calle", "SD.NUM_EXTERIOR AS numExterior",
-                        "SD.NUM_INTERIOR AS numInterior",
-                        "SD.REF_CP AS cp", "SD.REF_COLONIA AS colonia", "SD.REF_MUNICIPIO AS municipio",
-                        "SD.REF_ESTADO AS estado", "PA.DES_PAIS AS pais",
-                        "(SELECT COUNT(CPF.ID_CONTRA_PAQ_CONVENIO_PF) FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF WHERE CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE LIMIT 1) AS tieneConvenio",
-                        "(SELECT C.DES_FOLIO  FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF LEFT JOIN SVT_CONVENIO_PF C ON CPF.ID_CONVENIO_PF = C.ID_CONVENIO_PF WHERE CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE LIMIT 1) AS folioConvenio",
-                        "DATE_FORMAT((SELECT C.FEC_ALTA  FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF LEFT JOIN SVT_CONVENIO_PF C ON CPF.ID_CONVENIO_PF = C.ID_CONVENIO_PF where CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE LIMIT 1),'%d/%m/%Y') AS fecha")
-                        .from("SVC_CONTRATANTE SC")
-                        .leftJoin("SVC_PERSONA SP", "SC.ID_PERSONA = SP.ID_PERSONA")
-                        .leftJoin("SVT_DOMICILIO SD", "SC.ID_DOMICILIO = SD.ID_DOMICILIO")
-                        .leftJoin("SVC_PAIS PA", "SP.ID_PAIS = PA.ID_PAIS")
-                        .where("SP.CVE_RFC = '" + rfc+"'")
-                        .orderBy("SC.ID_CONTRATANTE DESC LIMIT 1");
-        String consulta = selectQueryUtilRfc.build();
-        log.info(consulta);
-        return consulta;
-	}
-	
-	
-	
 
+	public String consultarCurpRfc(String curp, String rfc) {
+		if (rfc.isEmpty() && rfc.equalsIgnoreCase("")) {
+			log.info("rfc vacio");
+			SelectQueryUtil selectQueryUtilCurp = new SelectQueryUtil();
+			selectQueryUtilCurp.select("SP.ID_PERSONA as idPersona", "SP.CVE_RFC AS rfc", "SP.CVE_CURP AS curp",
+					"SP.CVE_NSS AS nss",
+					"SP.NOM_PERSONA AS nomPersona",
+					"SC.ID_CONTRATANTE as idDelContratante",
+					"SP.NOM_PRIMER_APELLIDO AS primerApellido",
+					"SP.NOM_SEGUNDO_APELLIDO AS segundoApellido",
+					"SP.NUM_SEXO AS sexo", "IFNULL(SP.REF_OTRO_SEXO,'') AS otroSexo",
+					"SP.FEC_NAC AS fechaNacimiento",
+					"SP.ID_PAIS AS idPais", "SP.ID_ESTADO AS idEstado",
+					"SP.REF_TELEFONO AS telefono", "SP.REF_CORREO AS correo",
+					"SP.TIP_PERSONA AS tipoPersona",
+					"SD.REF_CALLE AS calle", "SD.NUM_EXTERIOR AS numExterior",
+					"SD.NUM_INTERIOR AS numInterior",
+					"SD.REF_CP AS cp", "SD.REF_COLONIA AS colonia", "SD.REF_MUNICIPIO AS municipio",
+					"SD.REF_ESTADO AS estado", "PA.DES_PAIS AS pais",
+					"(SELECT COUNT(CPF.ID_CONTRA_PAQ_CONVENIO_PF) FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF WHERE CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE  LIMIT 1) AS tieneConvenio",
+					"(SELECT C.DES_FOLIO  FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF LEFT JOIN SVT_CONVENIO_PF C ON CPF.ID_CONVENIO_PF = C.ID_CONVENIO_PF WHERE CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE  LIMIT 1) AS folioConvenio",
+					"DATE_FORMAT((SELECT C.FEC_ALTA  FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF LEFT JOIN SVT_CONVENIO_PF C ON CPF.ID_CONVENIO_PF = C.ID_CONVENIO_PF where CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE  LIMIT 1 ),'%d/%m/%Y') AS fecha")
+					.from("SVC_CONTRATANTE SC")
+					.leftJoin("SVC_PERSONA SP", "SC.ID_PERSONA = SP.ID_PERSONA")
+					.leftJoin("SVT_DOMICILIO SD", "SC.ID_DOMICILIO = SD.ID_DOMICILIO")
+					.leftJoin("SVC_PAIS PA", "SP.ID_PAIS = PA.ID_PAIS")
+					.where("SP.CVE_CURP = '" + curp + "'")
+					.orderBy("SC.ID_CONTRATANTE DESC LIMIT 1");
+			query = selectQueryUtilCurp.build();
+			log.info(query);
+
+			return query;
+		}
+		query = busquedaRfcCurp(rfc);
+		return query;
+	}
+
+	public String busquedaRfcCurp(String rfc) {
+		SelectQueryUtil selectQueryUtilRfc = new SelectQueryUtil();
+		selectQueryUtilRfc.select("SP.ID_PERSONA as idPersona", "SP.CVE_RFC AS rfc", "SP.CVE_CURP AS curp",
+				"SP.CVE_NSS AS nss",
+				"SP.NOM_PERSONA AS nomPersona",
+				"SC.ID_CONTRATANTE as idDelContratante",
+				"SP.NOM_PRIMER_APELLIDO AS primerApellido",
+				"SP.NOM_SEGUNDO_APELLIDO AS segundoApellido",
+				"SP.NUM_SEXO AS sexo", "IFNULL(SP.REF_OTRO_SEXO,'') AS otroSexo",
+				"SP.FEC_NAC AS fechaNacimiento",
+				"SP.ID_PAIS AS idPais", "SP.ID_ESTADO AS idEstado",
+				"SP.REF_TELEFONO AS telefono", "SP.REF_CORREO AS correo",
+				"SP.TIP_PERSONA AS tipoPersona",
+				"SD.REF_CALLE AS calle", "SD.NUM_EXTERIOR AS numExterior",
+				"SD.NUM_INTERIOR AS numInterior",
+				"SD.REF_CP AS cp", "SD.REF_COLONIA AS colonia", "SD.REF_MUNICIPIO AS municipio",
+				"SD.REF_ESTADO AS estado", "PA.DES_PAIS AS pais",
+				"(SELECT COUNT(CPF.ID_CONTRA_PAQ_CONVENIO_PF) FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF WHERE CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE LIMIT 1) AS tieneConvenio",
+				"(SELECT C.DES_FOLIO  FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF LEFT JOIN SVT_CONVENIO_PF C ON CPF.ID_CONVENIO_PF = C.ID_CONVENIO_PF WHERE CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE LIMIT 1) AS folioConvenio",
+				"DATE_FORMAT((SELECT C.FEC_ALTA  FROM SVT_CONTRA_PAQ_CONVENIO_PF CPF LEFT JOIN SVT_CONVENIO_PF C ON CPF.ID_CONVENIO_PF = C.ID_CONVENIO_PF where CPF.ID_CONTRATANTE = SC.ID_CONTRATANTE LIMIT 1),'%d/%m/%Y') AS fecha")
+				.from("SVC_CONTRATANTE SC")
+				.leftJoin("SVC_PERSONA SP", "SC.ID_PERSONA = SP.ID_PERSONA")
+				.leftJoin("SVT_DOMICILIO SD", "SC.ID_DOMICILIO = SD.ID_DOMICILIO")
+				.leftJoin("SVC_PAIS PA", "SP.ID_PAIS = PA.ID_PAIS")
+				.where("SP.CVE_RFC = '" + rfc + "'")
+				.orderBy("SC.ID_CONTRATANTE DESC LIMIT 1");
+		String consulta = selectQueryUtilRfc.build();
+		log.info(consulta);
+		return consulta;
+	}
 
 }
